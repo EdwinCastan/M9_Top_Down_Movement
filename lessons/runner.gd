cc221dd67a480c95c7be7453a051f69429bca330
EdwinCastan <edcastanedacalde@s.sfusd.edu>
2025-12-12 14:10:53 -0800

    M9l8

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,5 +1,7 @@
+class_name Runner
 extends CharacterBody2D
 
+signal walked_to
 ## The top speed that the runner can achieve
 @export var max_speed := 600.0
 ## How much speed is added per second when the player presses a movement key
@@ -34,3 +36,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+		
+func walk_to(destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name - RunnerVisual.Animations.WALK
+	_dust.emitting = true
+	
+	var distance := global_position.distance_to(destination_global_position)
+	var duration := distance / (max_speed * 0.2)
+	
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+)
